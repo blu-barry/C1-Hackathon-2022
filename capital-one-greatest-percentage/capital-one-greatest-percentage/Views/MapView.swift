1b2a0fa5c83a6e9f9cb19500f51713767d5673a8
Mike Barry <michael23barry@gmail.com>
2022-01-05 18:47:42 -0500

    Added file structure and MapView

--- capital-one-greatest-percentage/capital-one-greatest-percentage/Views/MapView.swift
+++ capital-one-greatest-percentage/capital-one-greatest-percentage/Views/MapView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MapView.swift
 //  capital-one-greatest-percentage
 //
 //  Created by Michael Barry on 1/5/22.
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MapView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MapView()
     }
 }
