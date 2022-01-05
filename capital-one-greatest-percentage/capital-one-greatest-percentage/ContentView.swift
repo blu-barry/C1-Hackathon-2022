378513298fb015bf1a1d73ac38a0a558e2a5325b
Mike Barry <michael23barry@gmail.com>
2022-01-05 17:26:24 -0500

    Initial commit with blank xcode project

---
+++ capital-one-greatest-percentage/capital-one-greatest-percentage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  capital-one-greatest-percentage
+//
+//  Created by mozzy on 1/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
