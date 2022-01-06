82c434c34839f5f2709f62e89a59e55e7d221cc9
Jonathan Wang <jfwang983@gmail.com>
2022-01-06 07:31:32 -0800

    API test code

--- capital-one-greatest-percentage/capital-one-greatest-percentage/ContentView.swift
+++ capital-one-greatest-percentage/capital-one-greatest-percentage/ContentView.swift
@@ -7,15 +7,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        MainView()
-        
-    }
+struct Course: Hashable, Codable {
+  let name: String
+  let image: String
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class ViewModel: ObservableObject {
+  @Published var courses: [Course] = []
+  func fetch() {
+    guard let url = URL(string:
+      "https://iosacademy.io/api/v1/courses/index.php") else {
+      return
     }
+    let task = URLSession.shared.dataTask(with: url) { [weak
+      self] data, _,error in
+      guard let data = data, error == nil else {
+        return
+      }
+      // Convert to JSON
+      do {
+        let courses = try JSONDecoder().decode([Course].self,
+          from: data)
+        DispatchQueue.main.async {
+          self?.courses = courses
+        }
+      }
+      catch {
+        print("Error")
+        print(error)
+      }
+    }
+    task.resume()
+  }
+}
+struct ContentView: View {
+  @StateObject var viewModel = ViewModel()
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(viewModel.courses, id: \.self) { course in
+          HStack {
+            Text(course.name).bold()
+          }.padding(3)
+        }
+      }
+      .onAppear {viewModel.fetch()}
+    }
+  }
+}
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
 }
